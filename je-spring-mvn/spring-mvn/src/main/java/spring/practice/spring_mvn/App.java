ebab1deb436800d38d50dabfa47078593b1efa50
Pritam Rajput <pritamrajput1993@gmail.com>
2020-09-25 22:51:10 +0530

    -

---
+++ je-spring-mvn/spring-mvn/src/main/java/spring/practice/spring_mvn/App.java
@@ -0,0 +1,13 @@
+package spring.practice.spring_mvn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
